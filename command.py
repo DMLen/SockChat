9cb79ed3388ac48d5067f7c32cc473e002e97c4f
DMLen <dmlennox03@gmail.com>
2024-06-14 02:45:21 +1000

    now child class of Message

--- command.py
+++ command.py
@@ -1,16 +1,9 @@
 from datetime import datetime
+from message import Message
 import pickle
 
-class Command():
-    def __init__(self, id, sender, content):
-        self.id = id
-        self.sender = sender
-        self.content = content
-        self.timestamp = datetime.now()
-        self.cmd = True 
 
-    def __str__(self):
-        return f"[{self.timestamp.strftime("%d/%m/%Y %H:%M:%S")}] {self.sender} sends command: {self.content}"
-    
-    def serialize(self):
-        return pickle.dumps(self)
+class Command(Message):
+    def __init__(self, id, sender, content):
+        super().__init__(id, sender, content)
+        self.cmd = True
